c247e6acbd12452687d1717d1d9220c93ca6c01b
zynbahmed <201901241@student.polytechnic.bh>
2024-01-31 16:03:01 +0300

    setting up

---
+++ express-animes/server.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+const axios = require('axios');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+pp.get('/', async (req, res) => {
+  const animeFactsUrl = 'https://catfact.ninja/facts';
+
+  try {
+    const response = await axios.get(animeFactsUrl);
+    const animeFacts = response.data.data;
+
+    res.render("index", {animeFacts})
+  } catch (error) {
+    console.error('Error fetching anime facts:', error);
+    res.status(500).send('Error fetching anime facts');
+  }
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
